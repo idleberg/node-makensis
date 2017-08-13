0d3ad9567b9f8976e2e46da97bfc79433f05d721
Jan T. Sott <jan@idleberg.com>
2017-08-13 16:50:01 +0200

    add test

---
+++ test/makensis.js
@@ -0,0 +1,60 @@
+const makensis = require('../dist/makensis');
+const path = require('path');
+const { spawnSync } = require('child_process');
+const { test } = require('tape');
+
+const script = path.join(__dirname, 'pass.nsi');
+const hasMakensis = spawnSync('makensis');
+
+let options = {};
+if (typeof hasMakensis.error !== 'undefined') {
+  options.skip = true;
+}
+
+test('Compile script [async]', options, (assert) => {
+  const expected = 0;
+  let actual;
+
+  makensis.compile(script)
+  .then(output => {
+      assert.equal(output.status, expected, 'should be equal');
+      assert.end();
+  }).
+  catch();
+});
+
+test('Compile script with error [async]', options, (assert) => {
+  const expected = 0;
+  let actual;
+
+  makensis.compile(script, {execute: ['!error']})
+  .then()
+  .catch(output => {
+      assert.notEqual(output.status, expected, 'should not be equal');
+      assert.end();
+  });
+});
+
+test('Compile script', options, (assert) => {
+  const expected = 0;
+  const actual = makensis.compileSync(script).status;
+
+  assert.equal(actual, expected, '- should be equal');
+  assert.end();
+});
+
+test('Get command help', options, (assert) => {
+  const expected = spawnSync('makensis', ['-CMDHELP', 'OutFile']).stdout.toString().trim();
+  const actual = makensis.helpSync('OutFile').stdout;
+
+  assert.equal(actual, expected, '- should be equal');
+  assert.end();
+});
+
+test('Print makensis version', options, (assert) => {
+  const expected = spawnSync('makensis', ['-VERSION']).stdout.toString().trim();
+  const actual = makensis.versionSync().stdout;
+
+  assert.equal(actual, expected, '- should be equal');
+  assert.end();
+});
