9e89fa45fb66e01fbce5456f2f1798fa45216a95
Jan T. Sott <jan@idleberg.com>
2017-08-13 19:13:55 +0200

    add tests

--- test/makensis.js
+++ test/makensis.js
@@ -1,17 +1,16 @@
+//Dependencies
 const makensis = require('../dist/makensis');
 const path = require('path');
 const { spawnSync } = require('child_process');
 const { test } = require('tape');
 
 // Generate script using compiler flags
-const script = {
-  execute: [
-    'OutFile test.exe',
-    'Section -default',
-    'Nop',
-    'SectionEnd'
-  ]
-};
+const execute = [
+  'OutFile test.exe',
+  'Section -default',
+  'Nop',
+  'SectionEnd'
+];
 
 // Skip test when makensis isn't installed
 const hasMakensis = spawnSync('makensis');
@@ -20,14 +19,14 @@ if (typeof hasMakensis.error !== 'undefined') {
   options.skip = true;
 }
 
-console.log('\nRunning Tape tests:');
+console.log('\nRunning Tape tests:\n');
 
 // Let's run the tests
 test('Print makensis version', options, (assert) => {
   const expected = spawnSync('makensis', ['-VERSION']).stdout.toString().trim();
   const actual = makensis.versionSync().stdout;
 
-  assert.equal(actual, expected, '- should be equal');
+  assert.equal(actual, expected, '');
   assert.end();
 });
 
@@ -35,7 +34,7 @@ test('Get command help', options, (assert) => {
   const expected = spawnSync('makensis', ['-CMDHELP']).stdout.toString().trim();
   const actual = makensis.cmdhelpSync().stdout;
 
-  assert.equal(actual, expected, '- should be equal');
+  assert.equal(actual, expected, '');
   assert.end();
 });
 
@@ -43,52 +42,86 @@ test('Get help for OutFile command', options, (assert) => {
   const expected = spawnSync('makensis', ['-CMDHELP', 'OutFile']).stdout.toString().trim();
   const actual = makensis.cmdhelpSync('OutFile').stdout;
 
-  assert.equal(actual, expected, '- should be equal');
+  assert.equal(actual, expected, '');
   assert.end();
 });
 
-test('Compile script [async]', options, (assert) => {
+test('Compilation [async]', options, (assert) => {
   const expected = 0;
 
-  makensis.compile(null, script)
+  makensis.compile(null, {execute: execute})
   .then(output => {
-      assert.equal(output.status, expected, 'should be equal');
+      assert.equal(output.status, expected, '');
       assert.end();
-  }).
-  catch();
+  })
+  .catch();
 });
 
-test('Compile script', options, (assert) => {
+test('Compilation with warning [async]', options, (assert) => {
+  const executeWithWarning = execute.concat(['!warning']);
   const expected = 0;
-  const actual = makensis.compileSync(null, script).status;
 
-  assert.equal(actual, expected, '- should be equal');
-  assert.end();
+  makensis.compile(null, {execute: executeWithWarning})
+  .then( output => {
+    assert.equal(output.status, expected, '');
+    assert.end();
+  })
+  .catch();
 });
 
-test('Compile script with error [async]', options, (assert) => {
-  let errorScript = script;
-  errorScript.execute.push('!error');
-  console.log(errorScript);
-
+test('Compilation with error [async]', options, (assert) => {
+  let executeWithError = execute.concat(['!error']);
   const expected = 0;
 
-  makensis.compile(null, errorScript)
-  .then()
+  makensis.compile(null, {execute: executeWithError})
   .catch(output => {
-      assert.notEqual(output.status, expected, 'should not be equal');
+      assert.notEqual(output.status, expected, '');
       assert.end();
   });
 });
 
-test('Compile script with error', options, (assert) => {
-  let errorScript = script;
-  errorScript.execute.push('!error');
-  console.log(errorScript);
-
+test('Compilation', options, (assert) => {
   const expected = 0;
-  const actual = makensis.compileSync(null, errorScript).status;
+  const actual = makensis.compileSync(null, {execute: execute}).status;
 
-  assert.notEqual(actual, expected, '- should not be equal');
+  assert.equal(actual, expected, '');
+  assert.end();
+});
+
+test('Compilation with warning', options, (assert) => {
+  const executeWithWarning = execute.concat(['!warning']);
+  const expected = 0;
+  const actual = makensis.compileSync(null, {execute: executeWithWarning}).status;
+
+  assert.equal(actual, expected, '');
+  assert.end();
+});
+
+test('Compilation with error', options, (assert) => {
+  const executeWithError = execute.concat(['!error']);
+  const expected = 0;
+  const actual = makensis.compileSync(null, {execute: executeWithError}).status;
+
+  assert.notEqual(actual, expected, '');
+  assert.end();
+});
+
+test('Strict compilation with warning [async]', options, (assert) => {
+  const executeWithWarning = execute.concat(['!warning']);
+  const expected = 0;
+
+  makensis.compile(null, {execute: executeWithWarning, strict: true})
+  .catch(output => {
+    assert.notEqual(output.status, expected, '');
+    assert.end();
+  });
+});
+
+test('Strict compilation with warning', options, (assert) => {
+  const executeWithWarning = execute.concat(['!warning']);
+  const expected = 0;
+  const actual = makensis.compileSync(null, {execute: executeWithWarning, strict: true}).status;
+
+  assert.notEqual(actual, expected, '');
   assert.end();
 });
