30d55e395d2946ff085cc1d500349fef24a6f460
idleberg <jan@idleberg.com>
2017-11-29 00:59:19 +0100

    move to separate file

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+export interface CompilerOptions {
+  // makensis
+  define?: Object;
+  execute?: Array<string>;
+  object?: boolean;
+  noCD?: boolean;
+  noConfig?: boolean;
+  inputCharset?: string;
+  outputCharset?: string;
+  pause?: boolean;
+  ppo?: boolean;
+  wine?: boolean;
+  safePPO?: boolean;
+  strict?: boolean;
+  verbose?: number;
+
+  // child_process
+  cwd?: string;
+  detached?: boolean;
+  shell?: string;
+
+  // library
+  pathToMakensis?: string;
+}
