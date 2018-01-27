b9b2dce878b16541014de08396fd594dfa93c48e
idleberg <jan@idleberg.com>
2018-01-27 19:42:43 +0100

    edit tests

--- test/wine.js
+++ test/wine.js
@@ -13,119 +13,109 @@ const scriptDefault = [
 
 // These test require NSIS to be setup properly, with makensis in your
 // PATH environmental variable
-// test('Wine found in PATH environmental variable', t => {
-//   const actual = spawnSync('which', ['wine']).stdout.toString().trim();
+test('Wine found in PATH environmental variable', t => {
+  const actual = spawnSync('which', ['wine']).stdout.toString().trim();
 
-//   t.not(actual, '');
-// });
+  t.not(actual, '');
+});
 
-// test('Wine: Print makensis version', t => {
-//   const expected = spawnSync('wine', ['makensis', '-VERSION']).stdout.toString().trim();
-//   const actual = makensis.versionSync({wine: true}).stdout;
+test('Wine: Print makensis version', t => {
+  const expected = spawnSync('wine', ['makensis', '-VERSION']).stdout.toString().trim();
+  const actual = makensis.versionSync({wine: true}).stdout;
 
-//   t.is(actual, expected);
-// });
+  t.is(actual, expected);
+});
 
-// test('Wine: Print makensis version [async]', t => {
-//   const expected = spawnSync('wine', ['makensis', '-VERSION']).stdout.toString().trim();
+test('Wine: Print makensis version [async]', t => {
+  const expected = spawnSync('wine', ['makensis', '-VERSION']).stdout.toString().trim();
 
-//   return Promise.resolve(makensis.version({wine: true}))
-//   .then(output => {
-//       t.is(output.stdout, expected);
-//   })
-//   .catch();
-// });
-
-// test('Wine: Print compiler information', t => {
-//   const expected = spawnSync('wine', ['makensis', '-HDRINFO']).stdout.toString().trim();
-//   const actual = makensis.hdrInfoSync({wine: true}).stdout;
-
-//   t.is(actual, expected);
-// });
-
-test('Wine Print compiler information [async]', t => {
-  const expected = spawnSync('wine', ['makensis', '-HDRINFO']).stdout.toString().trim();
-
-  return Promise.resolve(makensis.hdrInfo({wine: true}))
+  return Promise.resolve(makensis.version({wine: true}))
   .then(output => {
-      t.is(output.stderr, expected);
+      t.is(output.stdout, expected);
   })
   .catch();
 });
 
-// test('Wine: Print help for all commands', t => {
-//   const expected = spawnSync('wine', ['makensis', '-CMDHELP']).stdout.toString().trim();
-//   const actual = makensis.cmdHelpSync('', {wine: true}).stdout;
+test('Wine: Print compiler information', t => {
+  const expected = spawnSync('wine', ['makensis', '-HDRINFO']).stdout.toString().trim();
+  const actual = makensis.hdrInfoSync({wine: true}).stdout;
 
-//   t.is(actual, expected);
-// });
+  t.is(actual, expected);
+});
 
-// test('Wine: Print help for OutFile command', t => {
-//   const expected = spawnSync('wine', ['makensis', '-CMDHELP', 'OutFile']).stdout.toString().trim();
-//   const actual = makensis.cmdHelpSync('OutFile', {wine: true}).stdout;
+test('Wine: Print help for all commands', t => {
+  const expected = spawnSync('wine', ['makensis', '-CMDHELP']).stdout.toString().trim();
+  const actual = makensis.cmdHelpSync('', {wine: true}).stdout;
 
-//   t.is(actual, expected);
-// });
+  t.is(actual, expected);
+});
 
-// test('Wine: Compilation', t => {
-//   const actual = makensis.compileSync(null, {execute: scriptDefault, wine: true}).status;
+test('Wine: Print help for OutFile command', t => {
+  const expected = spawnSync('wine', ['makensis', '-CMDHELP', 'OutFile']).stdout.toString().trim();
+  const actual = makensis.cmdHelpSync('OutFile', {wine: true}).stdout;
 
-//   t.is(actual, 0);
-// });
+  t.is(actual, expected);
+});
 
-// test('Wine: Compilation [async]', t => {
-//   return Promise.resolve(makensis.compile(null, {execute: scriptDefault, wine: true}))
-//   .then(output => {
-//       t.is(output.status, 0);
-//   })
-//   .catch();
-// });
+test('Wine: Compilation', t => {
+  const actual = makensis.compileSync(null, {execute: scriptDefault, wine: true}).status;
 
-// test('Wine: Compilation with warning', t => {
-//   const scriptWithWarning = scriptDefault.concat(['!warning']);
-//   const actual = makensis.compileSync(null, {execute: scriptWithWarning, wine: true}).status;
+  t.is(actual, 0);
+});
 
-//   t.is(actual, 0);
-// });
+test('Wine: Compilation [async]', t => {
+  return Promise.resolve(makensis.compile(null, {execute: scriptDefault, wine: true}))
+  .then(output => {
+      t.is(output.status, 0);
+  })
+  .catch();
+});
 
-// test('Wine: Compilation with warning [async]', t => {
-//   const scriptWithWarning = scriptDefault.concat(['!warning']);
+test('Wine: Compilation with warning', t => {
+  const scriptWithWarning = scriptDefault.concat(['!warning']);
+  const actual = makensis.compileSync(null, {execute: scriptWithWarning, wine: true}).status;
 
-//   return Promise.resolve(makensis.compile(null, {execute: scriptWithWarning, wine: true}))
-//   .then( output => {
-//     t.is(output.status, 0)
-//   })
-//   .catch();
-// });
+  t.is(actual, 0);
+});
 
-// test('Wine: Compilation with error', t => {
-//   const scriptWithError = scriptDefault.concat(['!error']);
-//   const actual = makensis.compileSync(null, {execute: scriptWithError, wine: true}).status;
+test('Wine: Compilation with warning [async]', t => {
+  const scriptWithWarning = scriptDefault.concat(['!warning']);
 
-//   t.not(actual, 0);
-// });
+  return Promise.resolve(makensis.compile(null, {execute: scriptWithWarning, wine: true}))
+  .then( output => {
+    t.is(output.status, 0)
+  })
+  .catch();
+});
 
-// test('Wine: Compilation with error [async]', t => {
-//   let scriptWithError = scriptDefault.concat(['!error']);
+test('Wine: Compilation with error', t => {
+  const scriptWithError = scriptDefault.concat(['!error']);
+  const actual = makensis.compileSync(null, {execute: scriptWithError, wine: true}).status;
 
-//   return Promise.resolve(makensis.compile(null, {execute: scriptWithError, wine: true}))
-//   .catch(output => {
-//       t.not(output.status, 0);
-//   });
-// });
+  t.not(actual, 0);
+});
 
-// test('Wine: Strict compilation with warning', t => {
-//   const scriptWithWarning = scriptDefault.concat(['!warning']);
-//   const actual = makensis.compileSync(null, {execute: scriptWithWarning, wine: true, strict: true}).status;
+test('Wine: Compilation with error [async]', t => {
+  let scriptWithError = scriptDefault.concat(['!error']);
 
-//   t.not(actual, 0);
-// });
+  return Promise.resolve(makensis.compile(null, {execute: scriptWithError, wine: true}))
+  .catch(output => {
+      t.not(output.status, 0);
+  });
+});
 
-// test('Wine: Strict compilation with warning [async]', t => {
-//   const scriptWithWarning = scriptDefault.concat(['!warning']);
+test('Wine: Strict compilation with warning', t => {
+  const scriptWithWarning = scriptDefault.concat(['!warning']);
+  const actual = makensis.compileSync(null, {execute: scriptWithWarning, wine: true, strict: true}).status;
 
-//   return Promise.resolve(makensis.compile(null, {execute: scriptWithWarning, wine: true, strict: true}))
-//   .catch(output => {
-//     t.not(output.status, 0);
-//   });
-// });
+  t.not(actual, 0);
+});
+
+test('Wine: Strict compilation with warning [async]', t => {
+  const scriptWithWarning = scriptDefault.concat(['!warning']);
+
+  return Promise.resolve(makensis.compile(null, {execute: scriptWithWarning, wine: true, strict: true}))
+  .catch(output => {
+    t.not(output.status, 0);
+  });
+});
