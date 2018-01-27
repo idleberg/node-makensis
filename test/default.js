5747ce4c1e9511fe2e9ebbce340e148840a722d6
idleberg <jan@idleberg.com>
2018-01-27 17:19:14 +0100

    update tests

--- test/default.js
+++ test/default.js
@@ -35,15 +35,25 @@ test('Print makensis version [async]', t => {
   .catch();
 });
 
-// test('Print compiler information [async]', t => {
-//   const expected = spawnSync('makensis', ['-HDRINFO']).stdout.toString().trim();
+test('Print makensis version as JSON', t => {
+  let expected = spawnSync('makensis', ['-VERSION']).stdout.toString().trim();
+  let actual = makensis.versionSync({json: true}).stdout;
 
-//   return Promise.resolve(makensis.hdrInfo())
-//   .then(output => {
-//       t.is(output.stdout, expected);
-//   })
-//   .catch();
-// });
+  actual.version = `v${actual.version}`;
+  actual = JSON.stringify(actual);
+  expected = JSON.stringify({ version: expected });
+
+  t.is(actual, expected);
+});
+
+test('Print compiler information [async]', t => {
+  const expected = spawnSync('makensis', ['-HDRINFO']).stderr.toString().trim();
+
+  return Promise.resolve(makensis.hdrInfo())
+  .catch(output => {
+    t.is(output.stderr, expected);
+  });
+});
 
 test('Print compiler information', t => {
   const expected = spawnSync('makensis', ['-HDRINFO']).stdout.toString().trim();
@@ -59,9 +69,29 @@ test('Print help for all commands', t => {
   t.is(actual, expected);
 });
 
+test('Print help for all commands as JSON', t => {
+  let expected = spawnSync('makensis', ['-CMDHELP']).stderr.toString().trim();
+  let actual = makensis.cmdHelpSync(null, {json: true}).stderr;
+
+  actual = JSON.stringify(actual);
+  expected = JSON.stringify({help: expected });
+
+  t.is(actual, expected);
+});
+
 test('Print help for OutFile command', t => {
   const expected = spawnSync('makensis', ['-CMDHELP', 'OutFile']).stdout.toString().trim();
   const actual = makensis.cmdHelpSync('OutFile').stdout;
+
+  t.is(actual, expected);
+});
+
+test('Print help for OutFile command as JSON', t => {
+  let expected = spawnSync('makensis', ['-CMDHELP', 'OutFile']).stderr.toString().trim();
+  let actual = makensis.cmdHelpSync('OutFile', {json: true}).stderr;
+
+  actual = JSON.stringify(actual);
+  expected = JSON.stringify({help: expected });
 
   t.is(actual, expected);
 });
@@ -87,12 +117,29 @@ test('Compilation with warning', t => {
   t.is(actual, 0);
 });
 
+test('Compilation with warning as JSON', t => {
+  const scriptWithWarning = scriptDefault.concat(['!warning']);
+  const actual = makensis.compileSync(null, {execute: scriptWithWarning, json: true});
+
+  t.is(actual.warnings, true);
+});
+
 test('Compilation with warning [async]', t => {
   const scriptWithWarning = scriptDefault.concat(['!warning']);
 
   return Promise.resolve(makensis.compile(null, {execute: scriptWithWarning}))
   .then( output => {
     t.is(output.status, 0)
+  })
+  .catch();
+});
+
+test('Compilation with warning as JSON [async]', t => {
+  const scriptWithWarning = scriptDefault.concat(['!warning']);
+
+  return Promise.resolve(makensis.compile(null, {execute: scriptWithWarning, json: true}))
+  .then( output => {
+    t.is(output.warnings, true)
   })
   .catch();
 });
