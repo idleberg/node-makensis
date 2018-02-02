3c3e06b9178a066bbc6cc2ba417e6760be8f892e
idleberg <jan@idleberg.com>
2018-02-02 21:22:47 +0100

    temporary fallback

--- src/util.ts
+++ src/util.ts
@@ -46,6 +46,15 @@ const mapArguments = (args, options) => {
         p.args.push(`-X${key}`);
       });
     }
+  // Temporary Fallback
+  } else if (typeof options.execute !== 'undefined') {
+    if (typeof options.execute === 'string') {
+      p.args.push(`-X${options.execute}`);
+    } else {
+      options.execute.forEach((key) => {
+        p.args.push(`-X${key}`);
+      });
+    }
   }
 
   if (options.nocd === true || options.noCD === true) {
