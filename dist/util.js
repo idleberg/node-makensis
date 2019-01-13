9f30f3d9bfe1c000821a18af4788b2604e4ffd70
floatwork_jan <jan@floatwork.at>
2019-01-13 18:20:40 +0100

    change condition

--- dist/util.js
+++ dist/util.js
@@ -103,8 +103,8 @@ var hasWarnings = function (line) {
 };
 var formatOutput = function (stream, args, opts) {
     var _a;
-    if (args.includes('-CMDHELP') && !stream.stdout && stream.stderr) {
-        // Before NSIS 3.04, CMDHELP writes to stderr by default — let's fix this
+    if (args.includes('-CMDHELP') && stream.stdout.trim() !== '' && stream.stderr) {
+        // CMDHELP writes to stderr by default, let's fix this
         _a = [stream.stderr, ''], stream.stdout = _a[0], stream.stderr = _a[1];
     }
     if (opts.json === true) {
