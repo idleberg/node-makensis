8d9f19c9fa1a394d6b3be8678b06943d9c8bf0a8
idleberg <git@idleberg.com>
2020-11-05 00:33:32 +0100

    import SpawnOptions

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,33 +1,37 @@
-interface NsisCompilerOptions {
-  // makensis
-  define?: unknown;
-  inputCharset?: string;
-  json?: boolean;
-  noCD?: boolean;
-  noConfig?: boolean;
-  outputCharset?: string;
-  pause?: boolean;
-  postExecute?: string | string[];
-  preExecute?: string | string[];
-  priority?: number;
-  ppo?: boolean;
-  safePPO?: boolean;
-  strict?: boolean;
-  verbose?: number;
-  wine?: boolean;
+declare namespace makensis {
+  interface CompilerOptions {
+    // makensis
+    define?: unknown;
+    inputCharset?: string;
+    json?: boolean;
+    noCD?: boolean;
+    noConfig?: boolean;
+    outputCharset?: string;
+    pause?: boolean;
+    postExecute?: string | string[];
+    preExecute?: string | string[];
+    priority?: number;
+    ppo?: boolean;
+    safePPO?: boolean;
+    strict?: boolean;
+    verbose?: number;
+    wine?: boolean;
 
-  // library
-  pathToMakensis?: string;
+    // library
+    pathToMakensis?: string;
+  }
+
+  interface CompilerOutput {
+    status: number;
+    stdout: string;
+    stderr: string;
+    warnings: number;
+  }
+
+  interface StreamOptions {
+    stdout?: string;
+    stderr?: string;
+  }
 }
 
-interface NsisCompilerOutput {
-  status: number;
-  stdout: string;
-  stderr: string;
-  warnings: number;
-}
-
-interface NsisStreamOptions {
-  stdout?: string;
-  stderr?: string;
-}
+export = makensis;
