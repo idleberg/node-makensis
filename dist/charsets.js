c98732ccd5e2d0900c19fbb39214b831cc70a480
idleberg <git@idleberg.com>
2020-11-24 00:22:35 +0100

    build

--- dist/charsets.js
+++ dist/charsets.js
@@ -27,11 +27,11 @@ function __spreadArrays() {
 
 function createCommonjsModule(fn, basedir, module) {
 	return module = {
-	  path: basedir,
-	  exports: {},
-	  require: function (path, base) {
-      return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
-    }
+		path: basedir,
+		exports: {},
+		require: function (path, base) {
+			return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
+		}
 	}, fn(module, module.exports), module.exports;
 }
 
