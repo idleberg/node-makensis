645f019695dec109d95d9672adfb8006a9c3adb5
Jan T. Sott <git@idleberg.com>
2021-08-04 23:43:03 +0200

    Add MapArguments type

--- types/index.d.ts
+++ types/index.d.ts
@@ -34,9 +34,16 @@ declare namespace makensis {
     warnings: number;
   }
 
+  type MapArguments = [string, string[], MapArgumentOptions];
+
+  interface MapArgumentOptions {
+    json?: boolean;
+    wine?: boolean;
+  }
+
   interface StreamOptions {
-    stdout?: string;
-    stderr?: string;
+    stdout: string;
+    stderr: string;
   }
 }
 
