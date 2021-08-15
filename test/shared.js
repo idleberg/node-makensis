7aea9fae00af88cbda008fb1c6ede8b88249f38d
Jan T. Sott <git@idleberg.com>
2021-08-15 07:19:12 +0200

    add optional chaining operators

--- test/shared.js
+++ test/shared.js
@@ -6,19 +6,19 @@ let cp;
 const shared = {};
 
 cp = spawnSync('makensis', ['-CMDHELP']);
-shared['commandHelp'] = cp.stdout.toString().trim() || cp.stderr.toString().trim();
+shared['commandHelp'] = cp.stdout?.toString().trim() || cp.stderr?.toString().trim() || undefined;
 
 cp = spawnSync('makensis', ['-CMDHELP', 'OutFile']);
-shared['outFile'] = cp.stdout.toString().trim() || cp.stderr.toString().trim();
+shared['outFile'] = cp.stdout?.toString().trim() || cp.stderr?.toString().trim() || undefined;
 
 cp = spawnSync('makensis', ['-HDRINFO']);
-shared['headerInfo'] = cp.stdout.toString().trim() || cp.stderr.toString().trim();
+shared['headerInfo'] = cp.stdout?.toString().trim() || cp.stderr?.toString().trim() || undefined;
 
 cp = spawnSync('makensis', ['-LICENSE']);
-shared['license'] = cp.stdout.toString().trim() || cp.stderr.toString().trim();
+shared['license'] = cp.stdout?.toString().trim() || cp.stderr?.toString().trim() || undefined;
 
 cp = spawnSync('makensis', ['-VERSION']);
-shared['version'] = cp.stdout.toString().trim() || cp.stderr.toString().trim();
+shared['version'] = cp.stdout?.toString().trim() || cp.stderr?.toString().trim() || undefined;
 
 const nullDevice = platform() === 'win32' ? 'NUL' : '/dev/null';
 
