c43b3bd3259fe4bc027be53fa67d33f828bf9479
Jan T. Sott <git@idleberg.com>
2021-11-02 11:56:56 +0100

    fix tests

---
+++ test/wine/environment.test.cjs
@@ -0,0 +1,42 @@
+/* eslint-disable */
+const { nullDevice } = require('../shared');
+const MakeNSIS = require('../../dist/makensis.cjs');
+const path = require('path');
+const test = require('ava');
+const uuid = require('uuid').v4;
+
+const scriptFile = path.join(__dirname, '..', 'fixtures', 'env.nsi')
+const randomString = uuid();
+
+const defaultOptions = {
+  define: {
+      NULL_DEVICE: nullDevice,
+  },
+  verbose: 4,
+  wine: true
+};
+
+process.env['NSIS_APP_MAGIC_ENVIRONMENT_VARIABLE'] = randomString;
+
+// Let's run the tests
+test('Define magic environment variable', (t) => {
+  const { stdout } = MakeNSIS.compile.sync(scriptFile, defaultOptions);
+
+  const expected = true;
+  const actual = stdout.includes(randomString);
+
+  t.is(actual, expected);
+});
+
+test('Define magic environment variable [async]', async t => {
+  try {
+      const { stdout } = await MakeNSIS.compile(scriptFile, defaultOptions);
+
+      const expected = true;
+      const actual = stdout.includes(randomString);
+
+      t.is(actual, expected);
+  } catch ({ stderr }) {
+      t.fail(stderr);
+  }
+});
