9d119f8dc0743584a45767b4b27f8573507b184b
Jan T. Sott <git@idleberg.com>
2022-03-09 00:34:49 +0100

    update eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,30 @@
-/* eslint-disable */
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: [
-    '@typescript-eslint',
+    '@typescript-eslint'
   ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:json/recommended',
+    'plugin:json/recommended'
+  ],
+  globals: {
+    require: true
+  },
+  ignorePatterns: [
+    'test/**/*'
+  ],
+  "overrides": [
+    {
+      "files": ["tsconfig.json"],
+      "rules": {
+        "json/*": ["error", "allowComments"]
+      }
+    }
   ],
   rules: {
+    "@typescript-eslint/no-explicit-any": "off",
   }
 };
+
