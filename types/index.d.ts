2c349c9cfb8d7c4af1404ac9fd57d5f7c9cc69e9
Jan T. Sott <git@idleberg.com>
2022-09-28 00:15:04 +0200

    format using tabs

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,84 +1,84 @@
 declare namespace makensis {
-  interface CommandHelpOptions {
-    [key: string]: string;
-  }
+	interface CommandHelpOptions {
+		[key: string]: string;
+	}
 
-  interface CompilerOptions {
-    define?: DefineOptions;
-    env?: string | boolean;
-    inputCharset?: string;
-    json?: boolean;
-    noCD?: boolean;
-    noConfig?: boolean;
-    outputCharset?: string;
-    pause?: boolean;
-    postExecute?: string | string[];
-    preExecute?: string | string[];
-    priority?: 0 | '0' | 1 | '1' | 2 | '2' | 3 | '3' | 4 | '4' | 5 | '5';
-    ppo?: boolean;
-    rawArguments?: string;
-    safePPO?: boolean;
-    strict?: boolean;
-    verbose?: 0 | '0' | 1 | '1' | 2 | '2' | 3 | '3' | 4 | '4';
-    wine?: boolean;
+	interface CompilerOptions {
+		define?: DefineOptions;
+		env?: string | boolean;
+		inputCharset?: string;
+		json?: boolean;
+		noCD?: boolean;
+		noConfig?: boolean;
+		outputCharset?: string;
+		pause?: boolean;
+		postExecute?: string | string[];
+		preExecute?: string | string[];
+		priority?: 0 | '0' | 1 | '1' | 2 | '2' | 3 | '3' | 4 | '4' | 5 | '5';
+		ppo?: boolean;
+		rawArguments?: string;
+		safePPO?: boolean;
+		strict?: boolean;
+		verbose?: 0 | '0' | 1 | '1' | 2 | '2' | 3 | '3' | 4 | '4';
+		wine?: boolean;
 
-    // library
-    pathToMakensis?: string;
-    pathToWine?: string;
-  }
+		// library
+		pathToMakensis?: string;
+		pathToWine?: string;
+	}
 
-  interface CompilerOutput {
-    outFile?: string;
-    status: number;
-    stdout: string | HeaderInfo | HelpObject | Objectified;
-    stderr: string;
-    warnings: number;
-  }
+	interface CompilerOutput {
+		outFile?: string;
+		status: number;
+		stdout: string | HeaderInfo | HelpObject | Objectified;
+		stderr: string;
+		warnings: number;
+	}
 
-  interface DefineOptions {
-    [key: string]: string;
-  }
+	interface DefineOptions {
+		[key: string]: string;
+	}
 
-  interface EnvironmentVariables {
-    [key: string]: string | undefined;
-  }
+	interface EnvironmentVariables {
+		[key: string]: string | undefined;
+	}
 
-  interface HeaderInfo {
-    sizes: HeaderInfoSizes;
-    defined_symbols: HeaderInfoSymbols;
-  }
+	interface HeaderInfo {
+		sizes: HeaderInfoSizes;
+		defined_symbols: HeaderInfoSymbols;
+	}
 
-  interface HeaderInfoSizes {
-    [key: string]: string;
-  }
+	interface HeaderInfoSizes {
+		[key: string]: string;
+	}
 
-  interface HeaderInfoSymbols {
-    [key: string]: boolean | number | string;
-  }
+	interface HeaderInfoSymbols {
+		[key: string]: boolean | number | string;
+	}
 
-  interface HelpObject {
-    [key: string]: string;
-  }
+	interface HelpObject {
+		[key: string]: string;
+	}
 
-  type MapArguments = [string, string[], MapArgumentOptions];
+	type MapArguments = [string, string[], MapArgumentOptions];
 
-  interface MapArgumentOptions {
-    json?: boolean;
-    wine?: boolean;
-  }
+	interface MapArgumentOptions {
+		json?: boolean;
+		wine?: boolean;
+	}
 
-  interface StreamOptions {
-    stdout: string;
-    stderr: string;
-  }
-  interface StreamOptionsFormatted {
-    stdout: string | HeaderInfo | HelpObject | Objectified;
-    stderr: string;
-  }
+	interface StreamOptions {
+		stdout: string;
+		stderr: string;
+	}
+	interface StreamOptionsFormatted {
+		stdout: string | HeaderInfo | HelpObject | Objectified;
+		stderr: string;
+	}
 
-  interface Objectified {
-    [key: string]: boolean | number | string | undefined;
-  }
+	interface Objectified {
+		[key: string]: boolean | number | string | undefined;
+	}
 }
 
 export default makensis;
