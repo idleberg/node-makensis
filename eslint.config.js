db95743377f9fe4e9e65019803a62d44afdab1c0
Jan T. Sott <git@idleberg.com>
2024-11-17 23:29:38 +0100

    use strict rules

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
 	eslint.configs.recommended,
-	...tseslint.configs.recommended,
+	...tseslint.configs.strict,
 	...eslintPluginJsonc.configs['flat/recommended-with-json'],
 	{
 		plugins: {
